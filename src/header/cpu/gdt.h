7bbcb9e70aa659fe3781b3bb3dc21537339bde7d
Gryphuss <danielmuliaputraolo@gmail.com>
2024-03-04 12:48:45 +0700

    milestone 0

--- src/header/cpu/gdt.h
+++ src/header/cpu/gdt.h
@@ -35,6 +35,14 @@ struct SegmentDescriptor {
     uint8_t type_bit   : 4;
     uint8_t non_system : 1;
     // TODO : Continue SegmentDescriptor definition
+    uint8_t dpl        : 2;
+    uint8_t p          : 1;
+    uint8_t segment_limit;
+    uint8_t avl        : 1;
+    uint8_t l          : 1;
+    uint8_t dop        : 1;
+    uint8_t g          : 1;
+    uint8_t base_high;
 
 } __attribute__((packed));
 
