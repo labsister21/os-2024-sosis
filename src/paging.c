5afe349006a3313e050db6e3364fde8ff140736f
Ignatius Jhon Hezkiel Chan <kielchanyeol@gmail.com>
2024-04-30 23:04:27 +0700

    feat: add and combine paging (alr running)

--- src/paging.c
+++ src/paging.c
@@ -25,10 +25,8 @@ static struct PageManagerState page_manager_state = {
         [0]                            = true,
         [1 ... PAGE_FRAME_MAX_COUNT-1] = false
     },
-    .free_page_frame_count = PAGE_FRAME_MAX_COUNT - 1,
+    .free_page_frame_count = PAGE_FRAME_MAX_COUNT-1,
     .next_free_frame = 1,
-    .next_used_frame = 0
-    // TODO: Initialize page manager state properly
 };
 
 void update_page_directory_entry(
@@ -52,17 +50,16 @@ void flush_single_tlb(void *virtual_addr) {
 /* --- Memory Management --- */
 // TODO: Implement
 bool paging_allocate_check(uint32_t amount) {
-    // TODO: Check whether requested amount is available
-    uint32_t required_frame = amount / PAGE_FRAME_SIZE;
-    if (amount % PAGE_FRAME_SIZE != 0) {
-        required_frame++;
+    uint32_t req_page = amount / PAGE_FRAME_SIZE;
+    if(amount%PAGE_FRAME_SIZE!=0){
+        req_page++;
     }
-    return page_manager_state.free_page_frame_count >= required_frame;
+    return req_page<=page_manager_state.free_page_frame_count;
 }
 
 
 bool paging_allocate_user_page_frame(struct PageDirectory *page_dir, void *virtual_addr) {
-    /*
+    /**
      * TODO: Find free physical frame and map virtual frame into it
      * - Find free physical frame in page_manager_state.page_frame_map[] using any strategies
      * - Mark page_manager_state.page_frame_map[]
@@ -72,29 +69,33 @@ bool paging_allocate_user_page_frame(struct PageDirectory *page_dir, void *virtu
      *     > user bit       true
      *     > pagesize 4 mb  true
      */ 
-    uint32_t free_frame;
-
-    for (uint32_t i = page_manager_state.next_free_frame; i < PAGE_FRAME_MAX_COUNT; i++) {
-        if (!page_manager_state.page_frame_map[i]) {
-            page_manager_state.next_free_frame = i;
-            free_frame = i;
-        }
-        free_frame = (uint32_t) -1;
-    }
-
-    if (free_frame == (uint32_t) -1) {
+    if(page_manager_state.free_page_frame_count==0){
         return false;
     }
 
-    uint32_t page_index = ((uint32_t) virtual_addr >> 22) & 0x3FF;
-    page_dir->table[page_index].flag.present_bit = 1;
-    page_dir->table[page_index].flag.write_bit = 1;
-    page_dir->table[page_index].flag.user_bit = 1;
-    page_dir->table[page_index].flag.use_pagesize_4_mb = 1;
-    page_dir->table[page_index].lower_address = free_frame;
-    page_manager_state.page_frame_map[free_frame] = true;
+    int phy_mem_free = page_manager_state.next_free_frame;
     page_manager_state.free_page_frame_count--;
-    flush_single_tlb(virtual_addr);
+    page_manager_state.page_frame_map[phy_mem_free] = true;
+    
+    for(int i=phy_mem_free+1;i<PAGE_FRAME_MAX_COUNT;i++){
+        if(page_manager_state.page_frame_map[i]==false){
+            page_manager_state.next_free_frame = i;
+            break;
+        }
+    }
+    
+    struct PageDirectoryEntryFlag flag = {
+        .present_bit = 1,
+        .write_bit = 1,
+        .user_bit = 1,
+        .use_pagesize_4_mb = 1,
+    };
+    update_page_directory_entry(
+        page_dir,
+        (void*)&phy_mem_free,
+        (void*)&virtual_addr,
+        flag
+    );
     return true;
 }
 
@@ -105,15 +106,17 @@ bool paging_free_user_page_frame(struct PageDirectory *page_dir, void *virtual_a
      * - Remove the entry by setting it into 0
      */
     uint32_t page_index = ((uint32_t) virtual_addr >> 22) & 0x3FF;
-    uint32_t physical_frame = page_dir->table[page_index].lower_address;
-
-    if (page_manager_state.page_frame_map[physical_frame] == false) {
+    uint16_t phy_mapped = page_dir->table[page_index].lower_address;
+    if(!page_manager_state.page_frame_map[phy_mapped]){
         return false;
     }
 
-    page_manager_state.page_frame_map[physical_frame] = false;
-    page_manager_state.free_page_frame_count++;
     page_dir->table[page_index].lower_address = 0;
-    flush_single_tlb(virtual_addr);
+    page_manager_state.free_page_frame_count++;
+    page_manager_state.page_frame_map[phy_mapped] = false;
+    if(page_manager_state.next_free_frame>phy_mapped){
+        page_manager_state.next_free_frame = phy_mapped;
+    }
+
     return true;
 }
