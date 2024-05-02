ec727cc21c2296266955f38e187b7ea98b641d4e
Daniel M <danielmuliaputraolo@gmail.com>
2024-05-02 23:37:10 +0800

    fix case 1 - 3

--- src/user-shell.c
+++ src/user-shell.c
@@ -15,7 +15,7 @@ int main(void) {
     struct ClusterBuffer      cl[2]   = {0};
     struct FAT32DriverRequest request = {
         .buf                   = &cl,
-        .name                  = "shell",
+        .name                  = "metiuuuu",
         .ext                   = "\0\0\0",
         .parent_cluster_number = ROOT_CLUSTER_NUMBER,
         .buffer_size           = CLUSTER_SIZE,
@@ -23,8 +23,34 @@ int main(void) {
     int32_t retcode;
     syscall(0, (uint32_t) &request, (uint32_t) &retcode, 0);
     if (retcode == 0)
-        syscall(6, (uint32_t) "T", 1, 0xF);
-
+        syscall(6, (uint32_t) "R", 1, 0xF);
+    // =====================================
+    // syscall(1, (uint32_t) &request, (uint32_t) &retcode, 0);
+    // if (retcode == 0)
+    //     syscall(6, (uint32_t) "0", 1, 0xF);
+    // if (retcode == 1)
+    //     syscall(6, (uint32_t) "1", 1, 0xF);
+    // if (retcode == 2)
+    //     syscall(6, (uint32_t) "2", 1, 0xF);
+    // // =====================================
+    // syscall(2, (uint32_t) &request, (uint32_t) &retcode, 0);
+    // if (retcode == 0)
+    //     syscall(6, (uint32_t) "0", 1, 0xF);
+    // else if (retcode == 1)
+    //     syscall(6, (uint32_t) "1", 1, 0xF);
+    // else if (retcode == 2)
+    //     syscall(6, (uint32_t) "2", 1, 0xF);
+    // else if (retcode == -1)
+    //     syscall(6, (uint32_t) "9", 1, 0xF);
+    // // =====================================
+    // syscall(3, (uint32_t) &request, (uint32_t) &retcode, 0);
+    // if (retcode == 0)
+    //     syscall(6, (uint32_t) "0", 1, 0xF);
+    // else if (retcode == 1)
+    //     syscall(6, (uint32_t) "1", 1, 0xF);
+    // else if (retcode == 2)
+    //     syscall(6, (uint32_t) "2", 1, 0xF);
+    // =====================================
     char buf;
     syscall(7, 0, 0, 0);
     while (true) {
