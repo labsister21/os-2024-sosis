96afa0216b65cd11a451d45fd23978627b7f5db6
wigaandini <erdiantiwiga925@gmail.com>
2024-05-18 02:58:52 +0700

    feat: auto down

--- src/framebuffer.c
+++ src/framebuffer.c
@@ -7,6 +7,7 @@
 
 uint8_t cursor_col = 0;
 uint8_t cursor_row = 0;
+uint8_t max_written_row = 0;
 
 char terminal_buffer[BUFFER_HEIGHT][TERMINAL_WIDTH * 2];
 uint16_t view_row_start = 0;
@@ -15,12 +16,12 @@ void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg)
     if (c == '\n') {
         cursor_col = 0;
         cursor_row++;
-        if (cursor_row >= BUFFER_HEIGHT) {
-            cursor_row = BUFFER_HEIGHT - 1;
-            framebuffer_scroll_down();
+        if (cursor_row >= TERMINAL_HEIGHT) {
+            framebuffer_auto_down();
+            cursor_row = TERMINAL_HEIGHT - 1;
         }
-    } 
-    else {
+        framebuffer_set_cursor(cursor_row, cursor_col);
+    } else {
         if (col >= TERMINAL_WIDTH) {
             col = 0;
             row++;
@@ -28,23 +29,31 @@ void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg)
 
         if (row >= BUFFER_HEIGHT) {
             row = BUFFER_HEIGHT - 1;
-            framebuffer_scroll_down();
+            framebuffer_auto_down();
         }
 
         size_t loc = (row * 80 + col) * 2; // Menghitung posisi karakter c
         uint8_t color = (bg << 4) | (fg & 0X0F); // Memberi warna dan warna background pada karakter c
         terminal_buffer[row][col * 2] = c;
         terminal_buffer[row][col * 2 + 1] = color;
-
+        
         if (row >= view_row_start && row < view_row_start + TERMINAL_HEIGHT) {
             FRAMEBUFFER_MEMORY_OFFSET[loc] = c;
             FRAMEBUFFER_MEMORY_OFFSET[loc + 1] = color;
         }
 
-        // cursor_col++;
         if (cursor_col >= TERMINAL_WIDTH) {
             cursor_col = 0;
             cursor_row++;
+            if (cursor_row >= TERMINAL_HEIGHT) {
+                framebuffer_auto_down();
+                cursor_row = TERMINAL_HEIGHT - 1;
+            }
+        }
+
+        framebuffer_set_cursor(cursor_row, cursor_col);
+        if (row > max_written_row) {
+            max_written_row = row;
         }
     }
 }
@@ -74,6 +83,21 @@ void framebuffer_set_cursor(uint8_t r, uint8_t c) {
     cursor_col = c;
 }
 
+uint16_t framebuffer_get_cursor(void) {
+    /*
+    I.S. -
+    F.S. Mengembalikan posisi cursor
+    */
+    uint16_t pos = 0;
+    // Mengambil posisi cursor dari port CURSOR_PORT_CMD dan CURSOR_PORT_DATA
+    // Mengambil byte low cursor
+    out(CURSOR_PORT_CMD, 0x0F);
+    pos |= in(CURSOR_PORT_DATA);
+    // Mengambil byte high cursor
+    out(CURSOR_PORT_CMD, 0x0E);
+    pos = in(CURSOR_PORT_DATA) << 8;
+    return pos;
+}
 
 void framebuffer_clear(void) {
     /*
@@ -85,6 +109,7 @@ void framebuffer_clear(void) {
     view_row_start = 0; 
     cursor_row = 0;
     cursor_col = 0;
+    max_written_row = 0;
     framebuffer_set_cursor(cursor_row, cursor_col);
     // for (int i = 0; i < 80 * 25; i++) {
     //     FRAMEBUFFER_MEMORY_OFFSET[i+1] = 0X07;
@@ -93,13 +118,15 @@ void framebuffer_clear(void) {
 
 void framebuffer_update_view(void) {
     for (uint8_t r = 0; r < TERMINAL_HEIGHT; r++) {
+        uint8_t buffer_row = view_row_start + r;
         for (uint8_t c = 0; c < TERMINAL_WIDTH; c++) {
-            size_t loc = (r * 80 + c) * 2;
-            FRAMEBUFFER_MEMORY_OFFSET[loc] = terminal_buffer[view_row_start + r][c * 2];
-            FRAMEBUFFER_MEMORY_OFFSET[loc + 1] = terminal_buffer[view_row_start + r][c * 2 + 1];
+            size_t loc = (r * TERMINAL_WIDTH + c) * 2;
+            FRAMEBUFFER_MEMORY_OFFSET[loc] = terminal_buffer[buffer_row][c * 2];
+            FRAMEBUFFER_MEMORY_OFFSET[loc + 1] = terminal_buffer[buffer_row][c * 2 + 1];
         }
     }
 }
+
 
 void framebuffer_scroll_up(void) {
     if (view_row_start > 0) {
@@ -109,21 +136,48 @@ void framebuffer_scroll_up(void) {
 }
 
 void framebuffer_scroll_down(void) {
-    if (view_row_start < BUFFER_HEIGHT - TERMINAL_HEIGHT) {
+    if (view_row_start + TERMINAL_HEIGHT < max_written_row + 1) {
         view_row_start++;
         framebuffer_update_view();
     }
 }
 
-void handle_key_press(uint8_t scancode) {
-    switch (scancode) {
-        case 0x48: // arrow up
-            framebuffer_scroll_up();
-            break;
-        case 0x50: // arrow down
-            framebuffer_scroll_down();
-            break;
-        default:
-            break;
+
+void framebuffer_auto_down(void) {
+    if (view_row_start + TERMINAL_HEIGHT < BUFFER_HEIGHT) {
+        view_row_start++;
     }
+
+    // Move all rows up by one in the terminal buffer
+    for (uint8_t row = 0; row < TERMINAL_HEIGHT; row++) {
+        memcpy(FRAMEBUFFER_MEMORY_OFFSET + (row - 1) * TERMINAL_WIDTH * 2, 
+               FRAMEBUFFER_MEMORY_OFFSET + row * TERMINAL_WIDTH * 2, 
+               TERMINAL_WIDTH * 2);
+    }
+
+    // Clear the last row
+    for (uint8_t col = 0; col < TERMINAL_WIDTH; col++) {
+        size_t loc = ((TERMINAL_HEIGHT - 1) * TERMINAL_WIDTH + col) * 2;
+        FRAMEBUFFER_MEMORY_OFFSET[loc] = ' ';
+        FRAMEBUFFER_MEMORY_OFFSET[loc + 1] = 0x07; // Default color
+    }
+
+    // Adjust cursor position
+    cursor_row = TERMINAL_HEIGHT - 1;
+    cursor_col = 0;
+    framebuffer_set_cursor(cursor_row, cursor_col);
 }
+
+
+// void framebuffer_auto_down(void) {
+//     memmove(FRAMEBUFFER_MEMORY_OFFSET, FRAMEBUFFER_MEMORY_OFFSET + TERMINAL_WIDTH * 2, sizeof(uint16_t) * TERMINAL_WIDTH * (TERMINAL_HEIGHT - 1));
+
+//     for (uint8_t col = 0; col < TERMINAL_WIDTH; col++) {
+//         // size_t loc = ((TERMINAL_HEIGHT - 1) * TERMINAL_WIDTH + col) * 2;
+//         // FRAMEBUFFER_MEMORY_OFFSET[loc] = ' ';
+//         // FRAMEBUFFER_MEMORY_OFFSET[loc + 1] = 0x07; // Default color
+//         framebuffer_write(TERMINAL_HEIGHT - 1, col, ' ', 0x07, 0x00);
+//     }
+
+//     framebuffer_set_cursor(TERMINAL_HEIGHT - 1, 0);
+// }
--- src/keyboard.c
+++ src/keyboard.c
@@ -3,9 +3,6 @@
 #include "header/cpu/portio.h"
 #include "header/stdlib/string.h"
 
-
-static bool capslock_on = false;
-
 static char command[100] = "";
 static int idx = 0;
 
@@ -49,6 +46,13 @@ const char keyboard_scancode_caps_to_ascii_map[256] = {
       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
 };
 
+const char keyboard_scancode_shift_to_ascii_map[256] = {
+      0, 0x1B, '!', '@', '#', '$', '%', '^',  '&', '*', '(',  ')',  '_', '+', '\b', '\t',
+    'Q',  'W', 'E', 'R', 'T', 'Y', 'U', 'I',  'O', 'P', '{',  '}', '\n',   0,  'A',  'S',
+    'D',  'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '~',   0, '|',  'Z', 'X',  'C',  'V',
+    'B',  'N', 'M', '<', '>', '?',   0, '*',    0, ' ',   0,    0,    0,   0,    0,    0,
+};
+
 
 /**
  * KeyboardDriverState - Contain all driver states
@@ -61,6 +65,9 @@ struct KeyboardDriverState keyboard_state = {
     .read_extended_mode = false,
     .keyboard_input_on = false,
     .keyboard_buffer = '\0',
+    .capslock_on = false,
+    .shift_on = false,
+    .ctrl_on = false
 };
 
 
@@ -122,65 +129,80 @@ void delete_command(){
  */
 void keyboard_isr(void) {
     uint8_t scancode = in(KEYBOARD_DATA_PORT);
-    handle_key_press(scancode);
     char ascii_char;
-    if(keyboard_state.keyboard_input_on){
+    if (keyboard_state.keyboard_input_on) {
         bool is_break_code = (scancode & 0x80) != 0;
         if (is_break_code) {
             return;
         }
 
         if (scancode == CAPSLOCK_SCANCODE) {
-            capslock_on = !capslock_on;
+            keyboard_state.capslock_on = !keyboard_state.capslock_on;
             return;
         }
 
-        // if(keyboard_state.read_extended_mode){
-        if(scancode == EXT_SCANCODE_UP && cursor_row > 0){
-            cursor_row--;
-            framebuffer_set_cursor(cursor_row, cursor_col);
-            return;
+        if (scancode == LEFT_SHIFT_SCANCODE || scancode == RIGHT_SHIFT_SCANCODE) {
+            keyboard_state.shift_on = true;
+        } else if (scancode == LEFT_SHIFT_RELEASE_SCANCODE || scancode == RIGHT_SHIFT_RELEASE_SCANCODE) {
+            keyboard_state.shift_on = false;
+        } else if (scancode == CTRL_SCANCODE) {
+            keyboard_state.ctrl_on = true;
+        } else if (scancode == CTRL_RELEASE_SCANCODE) {
+            keyboard_state.ctrl_on = false;
         }
-        else if(scancode == EXT_SCANCODE_RIGHT && !(cursor_row == 24 && cursor_col == 79)){
-            if(cursor_col == 79){
-                cursor_row++;
-                cursor_col=0;
+
+        // Arrow key handling
+        if (scancode == EXT_SCANCODE_UP) {
+            if (cursor_row > 0) {
+                cursor_row--;
+                framebuffer_set_cursor(cursor_row, cursor_col);
+            } else if (view_row_start > 0) {
+                framebuffer_scroll_up();
             }
-            else{
+            return;
+        } else if (scancode == EXT_SCANCODE_DOWN) {
+            if (cursor_row < TERMINAL_HEIGHT - 1) {
+                cursor_row++;
+                framebuffer_set_cursor(cursor_row, cursor_col);
+            } else if (view_row_start < BUFFER_HEIGHT - TERMINAL_HEIGHT) {
+                framebuffer_scroll_down();
+            }
+            return;
+        } else if (scancode == EXT_SCANCODE_RIGHT && !(cursor_row == TERMINAL_HEIGHT - 1 && cursor_col == TERMINAL_WIDTH - 1)) {
+            if (cursor_col == TERMINAL_WIDTH - 1) {
+                cursor_row++;
+                cursor_col = 0;
+            } else {
                 cursor_col++;
             }
             framebuffer_set_cursor(cursor_row, cursor_col);
             return;
-        }
-        else if(scancode == EXT_SCANCODE_LEFT && !(cursor_row == 0 && cursor_col == 0)){
-            if(cursor_col == 0){
+        } else if (scancode == EXT_SCANCODE_LEFT && !(cursor_row == 0 && cursor_col == 0)) {
+            if (cursor_col == 0) {
                 cursor_row--;
-                cursor_col = 79;
+                cursor_col = TERMINAL_WIDTH - 1;
                 framebuffer_set_cursor(cursor_row, cursor_col);
-            }
-            else{
+            } else {
                 cursor_col--;
                 framebuffer_set_cursor(cursor_row, cursor_col);
             }
             return;
         }
-        else if(scancode == EXT_SCANCODE_DOWN && cursor_row < 25){
-            cursor_row++;
-            framebuffer_set_cursor(cursor_row, cursor_col);
-            return;
-        }
 
-        // char ascii_char;
-        if (capslock_on) {
+        // Character handling
+        if (keyboard_state.capslock_on) {
             ascii_char = keyboard_scancode_caps_to_ascii_map[scancode];
-        } 
-        else {
-            ascii_char = keyboard_scancode_1_to_ascii_map[scancode];
+        } else {
+            if (keyboard_state.shift_on) {
+                ascii_char = keyboard_scancode_shift_to_ascii_map[scancode];
+            } else {
+                ascii_char = keyboard_scancode_1_to_ascii_map[scancode];
+            }
         }
 
-        if (capslock_on && ascii_char >= 'a' && ascii_char <= 'z') {
-            ascii_char -= ('a' - 'A');
+        if (keyboard_state.capslock_on && ascii_char >= 'a' && ascii_char <= 'z') {
+            ascii_char += ('a' - 'A');
         }
         keyboard_state.keyboard_buffer = ascii_char;
-    } 
+    }
 }
